a40e280a08cb7594c8597bd00186f770c0cd7ee9
Mike McBride <mike.mcbride@hey.com>
2024-05-15 22:12:10 -0500

    fix plugin export

--- README.md
+++ README.md
@@ -34,14 +34,14 @@ You can configure the plugin like any other Telescope picker. You can see the av
 -- IMPORTANT!: this is only a showcase of how you can set default options!
 require("telescope").setup {
   extensions = {
-    telescope_mru = {
+    mru_files = {
       theme = "ivy",
     },
   },
 }
 -- To get telescope-mru loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
-require("telescope").load_extension "telescope_mru"
+require("telescope").load_extension "mru_files"
 ```
 
 ## Usage
@@ -49,17 +49,17 @@ require("telescope").load_extension "telescope_mru"
 You can use the `telescope-mru` plugin as follows:
 
 ```
-vim.keymap.set("n", "<space>sr", ":Telescope find_mru_files<CR>")
+vim.keymap.set("n", "<space>sr", ":Telescope mru_files<CR>")
 
 -- Alternatively, using lua API
-vim.keymap.set("n", "<space>fb", function()
-	require("telescope").extensions.telescope_mru.find_mru_files()
+vim.keymap.set("n", "<space>sr", function()
+	require("telescope").extensions.mru_files.mru_files({})
 end)
 ```
 
 ### Options
 
-The `find_mru_files` can take an options table. The default values are below:
+The `mru_files` can take an options table. The default values are below:
 
 ```
 mru_opts = {
@@ -70,5 +70,5 @@ mru_opts = {
 }
 ```
 
-- *ignore* is a function that takes a path and file extension and returns a boolean. It should return `true` if the file should be ignored from the list.
-- *max_items* is the maximum number of items the picker should show. If omitted, it will default to 50.
+- `ignore` is a function that takes a path and file extension and returns a boolean. It should return `true` if the file should be ignored from the list.
+- `max_items` is the maximum number of items the picker should show. If omitted, it will default to 50.
---
+++ lua/telescope/_extensions/mru_files.lua
@@ -0,0 +1,97 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local plenary_path = require("plenary.path")
+local cdir = vim.fn.getcwd()
+local if_nil = vim.F.if_nil
+
+local mru_files = function(opts)
+    opts = opts or {}
+
+    local function get_extension(fn)
+        local match = fn:match("^.+(%..+)$")
+        local ext = ""
+        if match ~= nil then
+            ext = match:sub(2)
+        end
+        return ext
+    end
+
+    local default_mru_ignore = { "gitcommit" }
+
+    local mru_opts = {
+        ignore = function(path, ext)
+            return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
+        end,
+        max_items = 50
+    }
+
+    local mru = function(cwd, local_opts)
+        local_opts = local_opts or mru_opts
+
+        -- default to 50 recent files if not present in options
+        local max_items = if_nil(local_opts.max_items, 50)
+
+        local oldfiles = {}
+        for _, v in pairs(vim.v.oldfiles) do
+            if #oldfiles == max_items then
+                break
+            end
+            local cwd_cond
+            if not cwd then
+                cwd_cond = true
+            else
+                cwd_cond = vim.startswith(v, cwd)
+            end
+            local ignore = (local_opts.ignore and local_opts.ignore(v, get_extension(v))) or false
+            if (vim.fn.filereadable(v) == 1) and cwd_cond and not ignore then
+                oldfiles[#oldfiles + 1] = v
+            end
+        end
+        local target_width = 35
+
+        local tbl = {}
+        for i, fn in ipairs(oldfiles) do
+            local short_fn
+            if cwd then
+                short_fn = vim.fn.fnamemodify(fn, ":.")
+            else
+                short_fn = vim.fn.fnamemodify(fn, ":~")
+            end
+
+            if #short_fn > target_width then
+                short_fn = plenary_path.new(short_fn):shorten(1, { -2, -1 })
+                if #short_fn > target_width then
+                    short_fn = plenary_path.new(short_fn):shorten(1, { -1 })
+                end
+            end
+            tbl[i] = { fn, short_fn }
+        end
+        return tbl
+    end
+
+    pickers.new(opts, {
+        prompt_title = "Recent Files",
+        finder = finders.new_table {
+            results = mru(cdir, opts),
+            entry_maker = function(entry)
+                return {
+                    value = entry,
+                    path = entry[1],
+                    display = entry[2],
+                    ordinal = entry[2],
+                }
+            end
+        },
+        sorter = conf.file_sorter(opts),
+    }):find()
+end
+
+return require("telescope").register_extension {
+    setup = function(ext_config, config)
+        -- access extension config and user config
+    end,
+    exports = {
+        mru_files = mru_files
+    },
+}
